613115c5e0139b23175a33b5cf727b0ec1e57e27
Bribb0L <50684429+Bribb0L@users.noreply.github.com>
2023-10-18 02:54:46 -0300

    Add Cart display and an option to remove items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,28 @@
-function Cart() {
+import PropTypes from 'prop-types'
+
+function Cart(props) {
+  function handleClickDelete(id) {
+    props.deleteFromCart(id);
+  }
+
   return (
     <>
       <h1>Cart component!</h1>
+      {props.cart.map((product) => (
+        <div key={product.id} className='product'>
+          <h5>{product.title}</h5>
+          <img src={product.image} style={{height: 5 +'%', width: 5 +'%'}} alt="" />
+          <p>{product.price}</p>
+          <p>{product.quantity}</p>
+          <button onClick={() => {handleClickDelete(product.id)}}>delete</button>
+        </div>
+      ))}
     </>
   )
+}
+
+Cart.propTypes = {
+
 }
 
 export default Cart
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Card from '../components/Card';
+import Cart from '../components/Cart';
 import { useEffect, useState } from 'react';
 
 let initialData = null;
@@ -27,22 +28,22 @@ function Shop() {
         .catch((error) => setError(error))
         .finally(() => console.log('finally'));
     }
-  }, []); 
+  }, []);
 
   function handleAddToCart(id, quantity) {
     let productIndex = -1;
-    const cartProduct = cart.find((product, index) =>{
+    const cartProduct = cart.find((product, index) => {
       if (product.id === id) {
         productIndex = index;
         return true;
       }
-      return false
+      return false;
     });
 
     if (!cartProduct) {
       // doesn't have the item
       let product = data.find((product) => product.id === id);
-      product = { ...product, quantity:  quantity  };
+      product = { ...product, quantity: quantity };
       setCart([...cart, product]);
     } else {
       // already has the item
@@ -52,24 +53,34 @@ function Shop() {
     }
   }
 
+  function deleteFromCart(id) {
+    //add confirm?
+    const newArr = cart.filter((product) => product.id != id);
+    setCart(newArr);
+  }
+
   return (
     <>
       <h1>this is ShopPage!</h1>
-
-      {data &&
-        data.map((product) => {
-          return (
-            <Card
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              img={product.image}
-              desc={product.description}
-              price={product.price}
-              handleAddToCart={handleAddToCart}
-            />
-          );
-        })}
+      <div className='cart'>
+        <Cart cart={cart} deleteFromCart={deleteFromCart} />
+      </div>
+      <div className="products">
+        {data &&
+          data.map((product) => {
+            return (
+              <Card
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                img={product.image}
+                desc={product.description}
+                price={product.price}
+                handleAddToCart={handleAddToCart}
+              />
+            );
+          })}
+      </div>
     </>
   );
 }
