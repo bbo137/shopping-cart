0013f4a8e56513b3625ed889d068a7ecc8c47deb
Bribb0L <50684429+Bribb0L@users.noreply.github.com>
2023-10-18 04:35:33 -0300

    Include Cart's total price

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,28 +1,43 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 function Cart(props) {
   function handleClickDelete(id) {
     props.deleteFromCart(id);
   }
 
+  function calcTotal() {
+    return parseFloat(
+      props.cart.reduce((acc, curr) => acc + curr.price * curr.quantity, 0)
+    ).toFixed(2);
+  }
+
   return (
     <>
       <h1>Cart component!</h1>
       {props.cart.map((product) => (
-        <div key={product.id} className='product'>
+        <div key={product.id} className="product">
           <h5>{product.title}</h5>
-          <img src={product.image} style={{height: 5 +'%', width: 5 +'%'}} alt="" />
+          <img
+            src={product.image}
+            style={{ height: 5 + '%', width: 5 + '%' }}
+            alt=""
+          />
           <p>{product.price}</p>
           <p>{product.quantity}</p>
-          <button onClick={() => {handleClickDelete(product.id)}}>delete</button>
+          <button
+            onClick={() => {
+              handleClickDelete(product.id);
+            }}
+          >
+            delete
+          </button>
         </div>
       ))}
+      <h6>total price: {calcTotal()}</h6>
     </>
-  )
+  );
 }
 
-Cart.propTypes = {
+Cart.propTypes = {};
 
-}
-
-export default Cart
+export default Cart;
